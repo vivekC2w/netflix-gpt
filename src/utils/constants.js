07e02ff326c8b3326ee5db227698f37fc5147451
Vivek23699 <vivek23699@gmail.com>
2024-01-12 08:27:33 +0530

    TMDB, Custom Hooks for playing movies, update store with movies data & trailer video data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,11 +5,11 @@ export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
 export const API_OPTIONS = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    accept: "application/json",
-    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
-  },
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNWQ3ZTQyNGYxNGY2MzY4NTQ5NDhmMTY2MDk3YzVmNCIsInN1YiI6IjY1OWZkYzk0MmUwNjk3MDEyNDc0NWVhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J0aQvJjUti6jCU6ro8T_aBJYwUbRIi7Rg11USFYgcfM'
+  }
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
