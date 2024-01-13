70ec5790d10cc2634081054a1cc2a5a2cfb61bb8
Vivek23699 <vivek23699@gmail.com>
2024-01-13 17:27:58 +0530

    UI bug fixed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,8 +4,8 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-4">
-      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
+    <div className="w-36 shrink-0 md:w-48 pr-4">
+      <img className="object-cover w-full" alt="Movie Card" src={IMG_CDN_URL + posterPath} />
     </div>
   );
 };
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,12 +5,11 @@ const MovieList = ({ title, movies }) => {
   return (
     <div className="px-6">
       <h1 className="text-lg md:text-3xl py-2 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll">
-        <div className="flex">
-          {movies?.map((movie) => (
-            <MovieCard key={movie?.id} posterPath={movie?.poster_path} />
-          ))}
-        </div>
+
+      <div className="flex overflow-x-scroll horizontalScrollbar">
+        {movies?.map((movie) => (
+          <MovieCard key={movie?.id} posterPath={movie?.poster_path} />
+        ))}
       </div>
     </div>
   );
